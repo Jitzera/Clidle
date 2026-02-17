47bd365db5cae92ff2be90cf5c5ab58c2548d747
Jitzera <jamal.hammad555@gmail.com>
2026-02-17 06:36:38 +0300

    added word comparison

--- cmd/Clidle/Clidle.go
+++ cmd/Clidle/Clidle.go
@@ -1,5 +1,46 @@
 package main
 
+import "fmt"
+
+var correctWord = ""
+var runedCorrect = []rune("")
+var guessedWord = ""
+var runedGuess = []rune("")
+
 func main() {
+	wordRNG()
+	wordScanner()
+}
+
+func wordRNG() {
+	correctWord = "water"
+	runedCorrect = []rune(correctWord)
+}
+
+func wordScanner() {
+	keyScan := ""
+	fmt.Scanln(&keyScan)
+	guessedWord = keyScan
+	runedGuess = []rune(guessedWord)
+
+	if len(guessedWord) == 5 {
+		println(guessedWord)
+		wordVerifier()
+	} else {
+		println("Invalid input.")
+	}
+}
+
+func wordVerifier() {
+	count := 0
+	for count < 5 {
+		//print(runedGuess[count])
+		//print(runedCorrect[count])
+		if runedGuess[count] == runedCorrect[count] {
+			println("c")
+		}
+		count++
+	}
+	println("lamo")
 
 }
